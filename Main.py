e2cde44882b0bb405b229972deeb2b451335f05b
Finbear2 <toastygamesstudio@gmail.com>
2025-05-30 10:27:18 +0100

    Finsihed Character Class

--- Main.py
+++ Main.py
@@ -5,7 +5,7 @@ Character_Amount = 0
 
 class Character:
     
-    def __init__(Self, Name, Class, Level, Background, Race, Alignment, Size, Human, Stats, Armor_Class, Initiative, Speed, Hit_Points, Hit_Dice, Proficiency_Bonus, Saving_Throws, Equipment, Spells, Inventory, Coin, Backstory, Traits, Features, Languages, Notes):
+    def __init__(Self, Name, Class, Level, Background, Race, Alignment, Size, Human, Stats, Armor_Class, Initiative, Speed, Hit_Points, Hit_Dice, Proficiency_Bonus, Saving_Throws, Equipment, Spells, Coins, Backstory, Traits, Features, Languages, Notes, Death_Saves):
         
         global Character_Amount
         
@@ -16,7 +16,22 @@ class Character:
         Self.Race = Race
         Self.Alignment = Alignment
         Self.Size = Size
-        Self.Human = Human
+        Self.Human = Human  
+        Self.Armor_Class = Armor_Class
+        Self.Initiative = Initiative
+        Self.Speed = Speed
+        Self.Hit_Points = Hit_Points
+        Self.Hit_Dice = Hit_Dice
+        Self.Proficiency_Bonus = Proficiency_Bonus
+        Self.Saving_Throws = Saving_Throws
+        Self.Equipment = Equipment
+        Self.Coins = Coins
+        Self.Backstory = Backstory
+        Self.Traits = Traits
+        Self.Features = Features
+        Self.Languages = Languages
+        Self.Notes = Notes
+        Self.Death_Saves = Death_Saves
         
         Self.Strength = Stats["Strength"]
         Self.Dexterity = Stats["Dexterity"]
@@ -25,9 +40,14 @@ class Character:
         Self.Wisdom = Stats["Wisdom"]
         Self.Charisma = Stats["Charisma"]
         
+        if Spells != None:
+            Self.Speed = Spells
+        else:
+            Self.Spells = "None"
+        
         Character_Amount += 1
         Self.ID = Character_Amount
- 
+        
 def Create_Stats( Strength, Dexterity, Constitution, Intelligence, Wisdom, Charisma ):
     
     return {
@@ -63,6 +83,27 @@ def Create_Saving_Rolls( Strength, Dexterity, Constitution, Intelligence, Wisdom
         "Charisma": Charisma
     }
        
+def Create_Coins(CP, EP, PP, GP, SP):
+    
+    return {
+        "Copper": CP,
+        "Electrum": EP,
+        "Platinum": PP,
+        "Gold": GP,
+        "Silver": SP
+    }
+    
+def Create_Equipment(Items, Amounts):
+    
+    Equipment = {}
+    
+    for item in range(len(Items)):
+        Equipment.append({Items[item]: Amounts[item]})
+        
+    return Equipment
+        
+        
+    
 def Quick_Character(Name, Class, Level, Race, Size, HP, Hit_Dice):
     
     Strength = randint(-1,3); Dexterity = randint(-1,3); Constitution = randint(-1,3); Intelligence = randint(-1,3); Wisdom = randint(-1,3); Charisma = randint(-1,3)
@@ -76,11 +117,22 @@ def Quick_Character(Name, Class, Level, Race, Size, HP, Hit_Dice):
     else:
         Speed = 40
     
-    return Character(Name, Class, Level, "None", Race, Alignment, Size, "None",
+    return Character(Name, Class, Level, "None", Race, "Neutral Good", Size, "None",
                      Create_Stats(randint(-1,3),Dexterity,randint(-1,3),randint(-1,3),randint(-1,3),randint(-1,3) ),
-                     10 + Dexterity, Dexterity, Speed, HP, Hit_Dice, 2 + (Level - 1) // 4,
+                     10 + ( Dexterity * 2 ), Dexterity, Speed, HP, Hit_Dice, 2 + (Level - 1) // 4,
                      Create_Saving_Rolls(Strength,Dexterity,Constitution,Intelligence,Wisdom,Charisma,Class,2 + (Level - 1) // 4),
                      "None", "None", "None", "None", "None", "None", "None", "None", "None")
+
+def Create_Character(Name, Class, Level, Race, Size, Background, Alignment, Human, Strength, Dexterity, Constitution, Intelligence, Wisdom, Charisma, HP, Hit_Dice, Equipment, Spells, Coins, Backstory, Traits, Features, Languages, Notes):
+    
+    Speed = None
+    
+    if any( r in Race for r in ["Human", "Elf", "Tiefling", "Tabaxi"]):
+        Speed = 30
+    elif any( r in Race for r in ["Dwarf", "Gnome", "Halfling"]):
+        Speed = 25
+    else:
+        Speed = 40
 
 Test_Character = Quick_Character("Test Character", "Fighter", 1, "Human", "Medium", 10, "1d10")
 

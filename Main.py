581e36fd440c56a4b8b27e342f6a0ed571f88210
Finbear2 <toastygamesstudio@gmail.com>
2025-05-30 10:35:18 +0100

    Finished Create Character Function

--- Main.py
+++ Main.py
@@ -25,13 +25,21 @@ class Character:
         Self.Proficiency_Bonus = Proficiency_Bonus
         Self.Saving_Throws = Saving_Throws
         Self.Equipment = Equipment
-        Self.Coins = Coins
         Self.Backstory = Backstory
         Self.Traits = Traits
         Self.Features = Features
         Self.Languages = Languages
         Self.Notes = Notes
         Self.Death_Saves = Death_Saves
+        
+        Self.Successes = Death_Saves["Success"]
+        Self.Failures = Death_Saves["Failure"]
+        
+        Self.CP = Coins["CP"]
+        Self.EP = Coins["EP"]
+        Self.PP = Coins["PP"]
+        Self.GP = Coins["GP"]
+        Self.SP = Coins["SP"]
         
         Self.Strength = Stats["Strength"]
         Self.Dexterity = Stats["Dexterity"]
@@ -121,9 +129,9 @@ def Quick_Character(Name, Class, Level, Race, Size, HP, Hit_Dice):
                      Create_Stats(randint(-1,3),Dexterity,randint(-1,3),randint(-1,3),randint(-1,3),randint(-1,3) ),
                      10 + ( Dexterity * 2 ), Dexterity, Speed, HP, Hit_Dice, 2 + (Level - 1) // 4,
                      Create_Saving_Rolls(Strength,Dexterity,Constitution,Intelligence,Wisdom,Charisma,Class,2 + (Level - 1) // 4),
-                     "None", "None", "None", "None", "None", "None", "None", "None", "None")
+                     "None", "None", "None", "None", "None", "None", "None", "None", "None", {"Success": 0, "Failure": 0})
 
-def Create_Character(Name, Class, Level, Race, Size, Background, Alignment, Human, Strength, Dexterity, Constitution, Intelligence, Wisdom, Charisma, HP, Hit_Dice, Equipment, Spells, Coins, Backstory, Traits, Features, Languages, Notes):
+def Create_Character(Name, Class, Level, Race, Size, Background, Alignment, Human, Strength, Dexterity, Constitution, Intelligence, Wisdom, Charisma, HP, Hit_Dice, Equipment, Spells, CP, SP, EP, GP, PP, Backstory, Traits, Features, Languages, Notes):
     
     Speed = None
     
@@ -133,7 +141,12 @@ def Create_Character(Name, Class, Level, Race, Size, Background, Alignment, Huma
         Speed = 25
     else:
         Speed = 40
+        
+    return Character(Name, Class, Level, Background, Race, Alignment, Size, Human, Create_Stats(Strength, Dexterity, Constitution, Intelligence, Wisdom, Charisma),
+                     10 + ( Dexterity * 2 ), Dexterity, Speed, HP, Hit_Dice, 2 + (Level - 1) // 4,
+                     Create_Saving_Rolls(Strength,Dexterity,Constitution,Intelligence,Wisdom,Charisma,Class,2 + (Level - 1) // 4),
+                     Equipment, Spells, Create_Coins(CP, EP, PP, GP, SP), Backstory, Traits, Features, Languages, Notes, {"Success": 0, "Failure": 0})
 
-Test_Character = Quick_Character("Test Character", "Fighter", 1, "Human", "Medium", 10, "1d10")
+Test_Character = Create_Character("Test", "Fighter", 1, "Human", "Medium", "Soldier", "Neutral Good", "Yes", 0, 0, 0, 0, 0, 0, 10, 1, "None", "None", 0, 0, 0, 10, 0, "Test Backstory", "Test Traits", "Test Features", "Common", "Test Notes")
 
 print(" Live Laugh Love ")

32fb5bb4b77e30ad118afe4e567c9b264deedc5a
Finbear2 <toastygamesstudio@gmail.com>
2025-05-30 10:59:41 +0100

    Improved Organization in PLayer Class and fixed uick character creator

--- Main.py
+++ Main.py
@@ -5,7 +5,24 @@ Character_Amount = 0
 
 class Character:
     
-    def __init__(Self, Name, Class, Level, Background, Race, Alignment, Size, Human, Stats, Armor_Class, Initiative, Speed, Hit_Points, Hit_Dice, Proficiency_Bonus, Saving_Throws, Equipment, Spells, Coins, Backstory, Traits, Features, Languages, Notes, Death_Saves):
+    class Coins:
+        def __init__(Self):
+            Self.CP = 0
+            Self.EP = 0
+            Self.PP = 0
+            Self.GP = 0
+            Self.SP = 0
+            
+    class Main_Stats:
+        def __init__(Self):
+            Self.Strength = 0
+            Self.Dexterity = 0
+            Self.Constitution = 0
+            Self.Intelligence = 0
+            Self.Wisdom = 0
+            Self.Charisma = 0
+    
+    def __init__(Self, Name, Class, Level, Background, Race, Alignment, Size, Human, Stats, Armor_Class, Initiative, Speed, Hit_Points, Hit_Dice, Proficiency_Bonus, Saving_Throws, Equipment, Spells, Currency, Backstory, Traits, Features, Languages, Notes, Death_Saves):
         
         global Character_Amount
         
@@ -35,18 +52,22 @@ class Character:
         Self.Successes = Death_Saves["Success"]
         Self.Failures = Death_Saves["Failure"]
         
-        Self.CP = Coins["CP"]
-        Self.EP = Coins["EP"]
-        Self.PP = Coins["PP"]
-        Self.GP = Coins["GP"]
-        Self.SP = Coins["SP"]
+        Self.Currency = Self.Coins()
         
-        Self.Strength = Stats["Strength"]
-        Self.Dexterity = Stats["Dexterity"]
-        Self.Constitution = Stats["Constitution"]
-        Self.Intelligence = Stats["Intelligence"]
-        Self.Wisdom = Stats["Wisdom"]
-        Self.Charisma = Stats["Charisma"]
+        Self.Currency.CP = Currency["Copper"]
+        Self.Currency.EP = Currency["Electrum"]
+        Self.Currency.PP = Currency["Platinum"]
+        Self.Currency.GP = Currency["Gold"]
+        Self.Currency.SP = Currency["Silver"]
+        
+        Self.Stats = Self.Main_Stats()
+        
+        Self.Stats.Strength = Stats["Strength"]
+        Self.Stats.Dexterity = Stats["Dexterity"]
+        Self.Stats.Constitution = Stats["Constitution"]
+        Self.Stats.Intelligence = Stats["Intelligence"]
+        Self.Stats.Wisdom = Stats["Wisdom"]
+        Self.Stats.Charisma = Stats["Charisma"]
         
         if Spells != None:
             Self.Speed = Spells
@@ -125,11 +146,7 @@ def Quick_Character(Name, Class, Level, Race, Size, HP, Hit_Dice):
     else:
         Speed = 40
     
-    return Character(Name, Class, Level, "None", Race, "Neutral Good", Size, "None",
-                     Create_Stats(randint(-1,3),Dexterity,randint(-1,3),randint(-1,3),randint(-1,3),randint(-1,3) ),
-                     10 + ( Dexterity * 2 ), Dexterity, Speed, HP, Hit_Dice, 2 + (Level - 1) // 4,
-                     Create_Saving_Rolls(Strength,Dexterity,Constitution,Intelligence,Wisdom,Charisma,Class,2 + (Level - 1) // 4),
-                     "None", "None", "None", "None", "None", "None", "None", "None", "None", {"Success": 0, "Failure": 0})
+    return Create_Character(Name, Class, Level, Race, Size, "None", "Neutral", "None", Strength, Dexterity, Constitution, Intelligence, Wisdom, Charisma, HP, Hit_Dice, "None", "None", 0, 0, 0, 10, 0, "None", "None", "None", "Common", "None")
 
 def Create_Character(Name, Class, Level, Race, Size, Background, Alignment, Human, Strength, Dexterity, Constitution, Intelligence, Wisdom, Charisma, HP, Hit_Dice, Equipment, Spells, CP, SP, EP, GP, PP, Backstory, Traits, Features, Languages, Notes):
     
@@ -147,6 +164,6 @@ def Create_Character(Name, Class, Level, Race, Size, Background, Alignment, Huma
                      Create_Saving_Rolls(Strength,Dexterity,Constitution,Intelligence,Wisdom,Charisma,Class,2 + (Level - 1) // 4),
                      Equipment, Spells, Create_Coins(CP, EP, PP, GP, SP), Backstory, Traits, Features, Languages, Notes, {"Success": 0, "Failure": 0})
 
-Test_Character = Create_Character("Test", "Fighter", 1, "Human", "Medium", "Soldier", "Neutral Good", "Yes", 0, 0, 0, 0, 0, 0, 10, 1, "None", "None", 0, 0, 0, 10, 0, "Test Backstory", "Test Traits", "Test Features", "Common", "Test Notes")
+Test_Character = Quick_Character("Test", "Fighter", 1, "Human", "Medium", 10, "1d10")
 
-print(" Live Laugh Love ")
+print("D&D Helper")
